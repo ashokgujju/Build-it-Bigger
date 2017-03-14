6de26aff44c76f0405c128ac9639ee18c01c5b72
ashok <ashokg.geek@gmail.com>
2017-03-14 17:07:43 +0530

    connect to gce from android

---
+++ jokesource/src/main/java/com/ashok/Jokes.java
@@ -0,0 +1,7 @@
+package com.ashok;
+
+public class Jokes {
+    public String getJoke() {
+        return "A computer once beat me at chess, but it was no match for me at kick boxing.";
+    }
+}
--- jokesource/src/main/java/com/example/Jokes.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Jokes {
-    public String getJoke() {
-        return "A computer once beat me at chess, but it was no match for me at kick boxing.";
-    }
-}
