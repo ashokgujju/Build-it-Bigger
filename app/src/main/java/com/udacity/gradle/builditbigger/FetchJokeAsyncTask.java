85eb88a6fdd2d008c938f6d052a49359dc4e1fc6
ashok <ashokg.geek@gmail.com>
2017-03-15 18:04:07 +0530

    add functionality test for asynctask

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/FetchJokeAsyncTask.java
@@ -0,0 +1,40 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.AsyncTask;
+
+import com.ashok.jokes.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+/**
+ * Created by ashok on 15/3/17.
+ */
+
+public class FetchJokeAsyncTask extends AsyncTask<Void, Void, String> {
+    private MyApi myApiService = null;
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        if (myApiService == null) {
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
+                            request.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+        try {
+            return myApiService.getJoke().execute().getData();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
--- backend/src/main/java/com/ashok/jokes/backend/MyEndpoint.java
+++ backend/src/main/java/com/ashok/jokes/backend/MyEndpoint.java
@@ -6,11 +6,10 @@
 
 package com.ashok.jokes.backend;
 
+import com.ashok.Jokes;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
 
 /**
  * An endpoint class we are exposing
@@ -25,15 +24,10 @@ import javax.inject.Named;
         )
 )
 public class MyEndpoint {
-
-    /**
-     * A simple endpoint method that takes a name and says Hi back
-     */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        response.setData(new Jokes().getJoke());
         return response;
     }
 
